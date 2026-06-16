2963f39fd6c60ea903648eab819209254383e5ad
pablormacia <pr.macia@gmail.com>
2026-06-16 20:20:01 -0300

    Creación de modelos

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+#ORM
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE
+    )
+
+    tags = models.ManyToManyField(
+        Tag
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published_date']
+
+    def clean(self):
+        if len(self.title) < 5:
+            raise ValidationError(
+                {'title': 'El título debe tener al menos 5 caracteres.'}
+            )
